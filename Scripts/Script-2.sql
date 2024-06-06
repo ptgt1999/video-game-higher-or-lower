de48e0ebebc7299c15305e8c4ba1e2c6324d0281
Ethan Robertson <ethanrobertsonc@gmail.com>
2024-06-06 18:49:52 -0400

    more database enteries

---
+++ Scripts/Script-2.sql
@@ -0,0 +1,2 @@
+insert into gamescores (game,score)
+values('The Legend of Zelda: Breath of the Wild','97')
---
+++ Scripts/Script.sql
@@ -0,0 +1,3 @@
+insert into gamescores (game,score)
+values('Baldurs Gate 3','96'),('Disco Elysium: The Final Cut','89')
+
