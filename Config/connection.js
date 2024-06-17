6b191e4595f512724e5cb11d12ec11bdeca23d76
Alyson-Sanchez <159204375+Alyson-Sanchez@users.noreply.github.com>
2024-06-17 18:22:59 -0400

    Update connection.js

--- Config/connection.js
+++ Config/connection.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+let sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
-    dialect: 'postgres'
+    host: 'dpg-cpo69508fa8c739mpdeg-a.ohio-postgres.render.com',
+    port:5432,
+    dialect: 'postgres',
+    dialectOptions:{
+      ssl:{
+      }
+    }
   }
 );
+
 
 module.exports = sequelize;
