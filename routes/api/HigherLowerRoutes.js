49e011a29cfbd7f6540a736ddd56ea04bf5582d1
Alyson-Sanchez <159204375+Alyson-Sanchez@users.noreply.github.com>
2024-06-17 18:25:38 -0400

    Update HigherLowerRoutes.js

--- routes/api/HigherLowerRoutes.js
+++ routes/api/HigherLowerRoutes.js
@@ -1,23 +1,25 @@
 const router = require('express').Router();
-const User = require('../../models/User.js');
-const gameData = require("../../seeds/gameChoiceData.json");
+//const User = require('../../models/User.js');
+//const gameData = require("../../seeds/gameChoiceData.json");
 const gameChoice = require('../../Models/GameChoice.js');
 
 
 // GET a Game
-router.get('/', async (req, res) => {
-  res.json(gameData);
+router.get('/getGameData', async (req, res) => {
+  console.log("test");
+  //use gamechoice modal to get rows > convert rows into json > send json string as a response to the api call
+
   // don't read js file, get info from database
-  return;
   try {
-    const userData = await User.findAll();
-    if (!userData) {
-      res.status(404).json({ message: 'No user with this id!' });
-      return;
+    const gameData = await gameChoice.findAll();
+    console.log(gameData)
+     if (!gameData) {
+       res.status(404).json({ message: 'No user with this id!' });
+       return;
     }
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json({ message: 'Internal server error!' });
+     res.status(200).json(gameData);
+   } catch (err) {
+     res.status(500).json({ message: 'Internal server error!' + err});
   }
 });
 
@@ -26,12 +28,12 @@ router.post('/', async (req, res) => {
   try {
   console.log(req.body);
   var jsonPost = json.parse(req.body);
-  gameChoice.create(`{ name: '${jsonPost.gameName}', score: '${jsonPost.gameScore}'}`);
+  gameChoice.create(`{ game: '${jsonPost.game}', score: '${jsonPost.score}'}`);
     res.status(204).end();
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-
+//
 module.exports = router;
