9a189e71b475e7a7c5c9cbb52d829947380ce99e
Alyson Sanchez <159204375+frogiington@users.noreply.github.com>
2024-06-20 13:26:11 -0400

    Update formScript.js

--- assets/js/formScript.js
+++ assets/js/formScript.js
@@ -2,9 +2,11 @@ function formInfo(gameName, gameScore){
     this.gameName = gameName;
     this.gameScore = gameScore;
 }
-
+console.log("test")
 var submit = document.getElementById("submit");
 submit.addEventListener("click", onSubmit)
+
+console.log(submit)
 
 function onSubmit(){
     console.log("form submit hit");
@@ -12,12 +14,34 @@ function onSubmit(){
     formInfoData.gameName = document.getElementById("gameName").value;
     formInfoData.gameScore = document.getElementById("gameScore").value;
 
-    fetch("/api/gamedata/",{
-        method:"POST",
+    console.log(formInfoData.gameName)
+    console.log(formInfoData.gameScore)
+try{
+    fetch("../api/gamedata/submitgame",{
+        method:'POST',
         body: JSON.stringify(formInfoData),
         headers:{
-            "Content-type": "application/json; charset=UTF-8"
+            'Content-type': 'application/json; charset=UTF-8'
         }
     })
-    .then(response => response.json())
+    .then((response)=>{console.log(response)
+    }).catch((error)=>{console.log(error)})
 }
+catch(error){
+    console.log(error)
+}
+fetch("localhost:3001/api/gamedata/getGameData",{mode:"same-origin"})
+.then(function(response){
+  console.log(response);
+  return response.json();
+})
+    // var request = require('request');
+    // request({uri:"/api/gamedata/submitgame",
+    //     body: JSON.stringify(formInfoData),
+    //     headers:{
+    //     'Content-type': 'application/json; charset=UTF-8'
+    //     }
+    // },function (error, response) {
+    //     console.log(error,response.body);
+    //     return;})
+    };
