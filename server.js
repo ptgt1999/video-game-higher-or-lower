7858f6f043c599cf43f9e483e69a3007502f8b9a
Patrick Gustafson <ptgt1999@gmail.com>
2024-06-20 20:02:09 -0400

    refactored code for final deployment

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./Config/connection.js');
-const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(cors({
-  origin:"http://127.0.0.1:3001",
-  methods:"GET,POST,PUT,DELETE",
-  credentials:true
-}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
